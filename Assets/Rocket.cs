7028374d54fb9863802fec844c11f78cdb9ca6e4
Diego Bao Montero <diego.bao@gmail.com>
2020-10-26 19:25:00 +0100

    added thurst and rotation modifiers

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -1,11 +1,17 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Animations;
 
 public class Rocket : MonoBehaviour
 {
     private Rigidbody rigidBody;
     private AudioSource audioSource;
+    [SerializeField]
+    private float rcsThrust = 100f;
+
+    [SerializeField] private float mainThrust = 1f;
     
     // Start is called before the first frame update
     void Start()
@@ -17,32 +23,64 @@ public class Rocket : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ProcessInput();
+        Thrust();
+        Rotate();
     }
 
-    private void ProcessInput()
+    private void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("Ok");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            case "Finished":
+                print("Finished");
+                break;
+            default:
+                print("Dead");
+                break;
+        }
+    }
+
+    private void Thrust()
     {
         if (Input.GetKey(KeyCode.Space))
         {
-            rigidBody.AddRelativeForce(Vector3.up);
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust);
             if (!audioSource.isPlaying)
             {
                 audioSource.Play();
             }
         }
-
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.Rotate(Vector3.forward );
-        } 
-        else if (Input.GetKey(KeyCode.D))
-        {
-            transform.Rotate(-Vector3.forward );
-        }
-
-        if (!Input.anyKey)
+        else
         {
             audioSource.Stop();
         }
+    }
+
+    private void Rotate()
+    {
+        rigidBody.freezeRotation = true; // take manual control of rotation
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            ApplyRotation(true);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            ApplyRotation(false);
+        }
+        
+        rigidBody.freezeRotation = false;
+    }
+
+    private void ApplyRotation(bool left)
+    {
+        var rotationThisFrame = rcsThrust * Time.deltaTime * (left ? 1 : - 1);
+        transform.Rotate(Vector3.forward * rotationThisFrame);
     }
 }
